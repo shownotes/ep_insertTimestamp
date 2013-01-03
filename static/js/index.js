70925f9a35ca88d942b3c2283f2088f080de10cf
mluto <m@luto.at>
2013-01-03 14:13:51 +0100

    Formatting, added repo-url

--- static/js/index.js
+++ static/js/index.js
@@ -1,13 +1,13 @@
 exports.aceEditEvent = function(hook, context)
 {
   if(context.callstack.type == "idleWorkTimer")
-	return;
+    return;
   
   var ace = context.editorInfo.editor;
   
   // Original by simonwaldherr, http://shownotes.github.com/EtherpadBookmarklets/
-  var padlines=ace.exportText().split('\n');
-  var timestamp=Math.round(new Date().getTime()/1000);
+  var padlines = ace.exportText().split('\n');
+  var timestamp = Math.round(new Date().getTime()/1000);
   
   for(var i = 0; i < padlines.length; i++)
   {
